e9f0b6ae0639f85f1a81d248bf1d1298bbafadfa
Oceane <grace@oceanespro.home>
2017-09-21 21:23:28 +0200

    petite stylisation

--- js/index.js
+++ js/index.js
@@ -1,3 +1,6 @@
 window.addEventListener("load", function() {
 	console.log("JS loade");
+	document.querySelector("#lifeBar").addEventListener("click", function(){
+		alert(this.querySelector("ratio").innerHTML);
+	},false);
 }, false);
